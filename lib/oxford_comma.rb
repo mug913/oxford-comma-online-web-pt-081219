ed2e5a0d8091bba40c4f1a9d54b7de6d453ce686
Brad Alexander <brad.alexander913@gmail.com>
2019-08-15 00:28:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+case array.size
+when 1 
+  array.join 
+  when 2 
+  array.join(" and ") 
+  when 3
+    hold = array.pop
+    last = "and " + hold
+    array << last
+    array.join(", ")
+  else 
+    hold = array.pop
+    last = "and " + hold
+    array << last
+    array.join(", ")
+  end
 end
